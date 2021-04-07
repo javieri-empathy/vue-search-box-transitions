e397a34f440a9f4bc37a6a389b238167329b3ef6
Javier Iglesias Garcia <javieri@empathy.co>
2021-04-07 11:42:04 +0200

    Remove SearchIcon.vue animation

--- src/components/SearchIcon.vue
+++ src/components/SearchIcon.vue
@@ -8,21 +8,12 @@
     xmlns="http://www.w3.org/2000/svg"
   >
     <circle cx="11" cy="11" r="8" />
-    <transition mode="out-in" name="search-loading-" :duration="500">
-      <g v-if="isLoading" class="search-icon__spinner">
-        <circle class="search-icon__spinner-bubble" cx="7" cy="11" r="0.25" />
-        <circle class="search-icon__spinner-bubble" cx="11" cy="11" r="0.25" />
-        <circle class="search-icon__spinner-bubble" cx="15" cy="11" r="0.25" />
-      </g>
-      <line
-        v-else
-        class="search-icon__handle"
-        x1="21"
-        x2="11"
-        y1="21"
-        y2="11"
-      />
-    </transition>
+    <g v-if="isLoading" class="search-icon__spinner">
+      <circle class="search-icon__spinner-bubble" cx="7" cy="11" r="0.25" />
+      <circle class="search-icon__spinner-bubble" cx="11" cy="11" r="0.25" />
+      <circle class="search-icon__spinner-bubble" cx="15" cy="11" r="0.25" />
+    </g>
+    <line v-else class="search-icon__handle" x1="21" x2="11" y1="21" y2="11" />
   </svg>
 </template>
 
@@ -54,57 +45,6 @@ export default Vue.extend({
 
   &__handle {
     stroke-dasharray: 6.15 15;
-  }
-}
-
-.search-icon {
-  &__spinner {
-    animation: rotate 1s ease-in-out infinite;
-  }
-
-  &__handle.search-loading {
-    &--enter,
-    &--leave-to {
-      stroke-dashoffset: -14;
-    }
-    &--leave-active {
-      transition: stroke-dashoffset 0.25s ease-in;
-    }
-    &--enter-active {
-      transition: stroke-dashoffset 0.25s ease-out;
-    }
-  }
-
-  &__spinner.search-loading {
-    &--enter-active {
-      .search-icon__spinner-bubble {
-        transition: transform 0.25s var(--ease-out-back);
-      }
-    }
-
-    &--leave-active {
-      .search-icon__spinner-bubble {
-        transition: transform 0.25s var(--ease-in-back);
-      }
-    }
-
-    &--enter,
-    &--leave-to {
-      .search-icon__spinner-bubble {
-        &:nth-child(1) {
-          transform: translateX(4px);
-        }
-        &:nth-child(3) {
-          transform: translateX(-4px);
-        }
-      }
-    }
-  }
-}
-
-@keyframes rotate {
-  to {
-    transform: rotateZ(360deg);
   }
 }
 </style>
